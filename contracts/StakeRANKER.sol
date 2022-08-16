481e204565e5594296d61799d56c237032c1a1a1
Ridwan Maulana Tanjung <rdtanx@gmail.com>
2022-08-16 17:42:40 +0700

    staker contract: define properties and constructor

--- contracts/StakeRANKER.sol
+++ contracts/StakeRANKER.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
 interface Token {
     function transfer(address receipent, uint256 amount)
         external
@@ -13,4 +18,40 @@ interface Token {
         address receipent,
         uint256 amount
     ) external returns (uint256);
+}
+
+contract StakeRANKER is Pausable, Ownable, ReentrancyGuard {
+    Token rankerToken;
+
+    // 30 days
+    uint256 public planDuration = 30 * 24 * 60 * 60;
+    // 180 days
+    uint256 _planExpired = 180 * 24 * 60 * 60;
+
+    uint8 public interestRate = 1 << 5;
+    uint256 public planExpired;
+    uint8 public totalStakers;
+
+    struct StakeInfo {
+        uint256 startTS;
+        uint256 endTS;
+        uint256 amount;
+        uint256 claimed;
+    }
+
+    event Staked(address indexed from, uint256 amount);
+    event Claimed(address indexed from, uint256 amount);
+
+    mapping(address => StakeInfo) public stakeInfos;
+    mapping(address => bool) public addressStaked;
+
+    constructor(Token _tokenAddress) {
+        require(
+            address(_tokenAddress) != address(0),
+            "Invalid address: address cannot be 0"
+        );
+        rankerToken = _tokenAddress;
+        _planExpired = block.timestamp + _planExpired;
+        totalStakers = 0;
+    }
 }
