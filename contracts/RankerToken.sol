1c31b1262cc08f8f035b71a121e26c9622675244
Ridwan Maulana Tanjung <rdtanx@gmail.com>
2022-08-16 17:07:45 +0700

    init project

---
+++ contracts/RankerToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RankerToken is ERC20 {
+    uint256 constant _totalSupply = 1e2 * 1e6 * 1e18; // 100m tokens for distribution
+
+    constructor() ERC20("RankerToken", "RANKER") {
+        _mint(msg.sender, _totalSupply);
+    }
+}
