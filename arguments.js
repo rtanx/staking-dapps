5dab275de8d2251c025f831792ccff5a7c926061
Ridwan Maulana Tanjung <rdtanx@gmail.com>
2022-09-19 12:45:06 +0700

    deploying and verifying

--- arguments.js
+++ arguments.js
@@ -2,5 +2,5 @@ module.exports = [
     "Ranker Badge",
     "RNKR",
     "https://ipfs.io/ipfs/bafybeihjjkwdrxxjnuwevlqtqmh3iegcadc32sio4wmo7bv2gbf34qs34a/{id}.json",
-    "0xa38aD40d281c1B0Fc2C342A98E35f6020Fed35Ed"
+    "0xbb3B92333bc2F429d72dF0F7d6D065fBaE9F88c9"
 ]
